6b80fe432e07815609d6d32356f7ed35e90cca83
ayas <ayyashmumtaz2003@gmail.com>
2023-05-21 08:42:00 +0700

    add library : express validator

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
 //import route posts
 const kerjaanRouter = require('./routes/data_outdoor');
 app.use('/api/pekerjaan', kerjaanRouter); // use route posts di Express
